cd149a645e659c95f0cbadc93bb299d62d9c916c
Cory Jett <cory.jett@gmail.com>
2024-12-03 09:35:07 -0500

    updates

---
+++ 2.sh
@@ -0,0 +1,159 @@
+#Setup for next section
+
+kubectl apply --context ${CLUSTER1} -f - <<EOF
+apiVersion: enterprise.gloo.solo.io/v1
+kind: AuthConfig
+metadata:
+  name: oauth
+  namespace: httpbin
+spec:
+  configs:
+    - oauth2:
+        oidcAuthorizationCode:
+          appUrl: "https://httpbin.cluster1.${_SANDBOX_ID}.instruqt.io"
+          callbackPath: /callback
+          clientId: ${KEYCLOAK_CLIENT}
+          clientSecretRef:
+            name: oauth
+            namespace: gloo-system
+          issuerUrl: "${KEYCLOAK_URL}/realms/workshop/"
+          logoutPath: /logout
+          afterLogoutUrl: "https://httpbin.cluster1.${_SANDBOX_ID}.instruqt.io/get"
+          session:
+            failOnFetchFailure: true
+            redis:
+              cookieName: keycloak-session
+              options:
+                host: redis:6379
+          scopes:
+          - email
+          headers:
+            idTokenHeader: jwt
+          identityToken:
+            claimsToHeaders:
+              - claim: email
+                header: X-Email
+EOF
+
+kubectl apply --context ${CLUSTER1} -f - <<EOF
+apiVersion: gateway.solo.io/v1
+kind: RouteOption
+metadata:
+  name: routeoption
+  namespace: httpbin
+spec:
+  options:
+    extauth:
+      configRef:
+        name: oauth
+        namespace: httpbin
+EOF
+
+kubectl apply --context ${CLUSTER1} -f - <<EOF
+apiVersion: gateway.networking.k8s.io/v1
+kind: HTTPRoute
+metadata:
+  name: httpbin
+  namespace: httpbin
+spec:
+  rules:
+    - matches:
+      - path:
+          type: PathPrefix
+          value: /
+      filters:
+        - type: ExtensionRef
+          extensionRef:
+            group: gateway.solo.io
+            kind: RouteOption
+            name: routeoption
+      backendRefs:
+        - name: httpbin1
+          port: 8000
+EOF
+
+kubectl apply --context ${CLUSTER1} -f - <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: allow-solo-email-users
+  namespace: httpbin
+data:
+  policy.rego: |-
+    package test
+
+    default allow = false
+
+    allow {
+        [header, payload, signature] = io.jwt.decode(input.state.jwt)
+        endswith(payload["email"], "@solo.io")
+    }
+EOF
+
+kubectl apply --context ${CLUSTER1} -f - <<EOF
+apiVersion: enterprise.gloo.solo.io/v1
+kind: AuthConfig
+metadata:
+  name: oauth
+  namespace: httpbin
+spec:
+  configs:
+    - oauth2:
+        oidcAuthorizationCode:
+          appUrl: "https://httpbin.cluster1.${_SANDBOX_ID}.instruqt.io"
+          callbackPath: /callback
+          clientId: ${KEYCLOAK_CLIENT}
+          clientSecretRef:
+            name: oauth
+            namespace: gloo-system
+          issuerUrl: "${KEYCLOAK_URL}/realms/workshop/"
+          logoutPath: /logout
+          afterLogoutUrl: "https://httpbin.cluster1.${_SANDBOX_ID}.instruqt.io/get"
+          session:
+            failOnFetchFailure: true
+            redis:
+              cookieName: keycloak-session
+              options:
+                host: redis:6379
+          scopes:
+          - email
+          headers:
+            idTokenHeader: jwt
+          identityToken:
+            claimsToHeaders:
+              - claim: email
+                header: X-Email
+    - opaAuth:
+        modules:
+        - name: allow-solo-email-users
+          namespace: httpbin
+        query: "data.test.allow == true"
+EOF
+
+kubectl apply --context ${CLUSTER1} -f - <<EOF
+apiVersion: gateway.solo.io/v1
+kind: RouteOption
+metadata:
+  name: routeoption
+  namespace: httpbin
+spec:
+  options:
+    extauth:
+      configRef:
+        name: oauth
+        namespace: httpbin
+    stagedTransformations:
+      regular:
+        requestTransforms:
+        - requestTransformation:
+            transformationTemplate:
+              extractors:
+                organization:
+                  header: 'X-Email'
+                  regex: '.*@(.*)$'
+                  subgroup: 1
+              headers:
+                x-organization:
+                  text: "{{ organization }}"
+EOF
+
